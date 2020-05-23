30ec12bb549bdcee5d68d2f99d60ea9c63405a8d
SURYANSH <suryanshtherockstar@gmail.com>
2020-05-23 09:45:17 +0530

    pca_feature

--- classification.py
+++ classification.py
@@ -7,20 +7,28 @@ Created on Fri May 22 11:16:39 2020
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-dataset =pd.read_csv('kidney_disease.csv')
-dataset =dataset.drop('id',1)
-x=dataset.iloc[:, :-1].values
-y=dataset.iloc[:, 24].values
 
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values= 'NaN' , strategy ='mean' , axis= 0)
-imputer.fit(x[:, 0:3])
-x[:, 0:3] = imputer.transform(x[:, 0:3])
-imputer1 = Imputer(missing_values='NaN' , strategy='mean' , axis=0)
-imputer1.fit(x[:, 9:18])
-x[:, 9:18]=imputer1.transform(x[:, 9:18])
-from sklearn_pandas import CategoricalImputer
-imputer2=CategoricalImputer()
-x[:, 3:9]=imputer2.fit_transform(x[:, 3:9])
+dataset =pd.read_csv('SPECTF_HeartDataSet.csv')
+x=dataset.iloc[:, :-1].values
+y=dataset.iloc[:, 44].values
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc_x=StandardScaler()
+x_train=sc_x.fit_transform(x_train)
+x_test= sc_x.transform(x_test)
+
+#from sklearn.decomposition import PCA
+#pca =PCA(n_components= None)
+#x_train=pca.fit_transform(x_train)
+#x_test=pca.transform(x_test)
+#ev=pca.explained_variance_ratio_*/
+from sklearn.decomposition import PCA
+pca =PCA(n_components= 5)
+x_train=pca.fit_transform(x_train)
+x_test=pca.transform(x_test)
+ev=pca.explained_variance_ratio_
 
 
